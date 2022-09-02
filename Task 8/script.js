d85df3e273a925e675d53b187a63bd8a44df8ea5
Petriukas <aistis.jampolskisu@gmail.com>
2022-09-02 22:38:16 +0300

    shows result in html ans console log

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,7 +5,12 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
-
+const n1 = 1;
+const n2 = 2;
+const sum = n1 + n2;
+const sub = n1 - n2;
+const div = n1 / n2;
+const multi = n1 * n2;
 function calculate(n1, n2, operator) {
   switch (operator) {
     case "sum":
@@ -24,3 +29,8 @@ console.log(calculate(1,2, "sum"));
 console.log(calculate(1,2, "sub"));
 console.log(calculate(1,2, "div"));
 console.log(calculate(1,2, "multi"));
+
+document.getElementById('a').innerHTML = 'Sudėtis ' +  n1 + " ir " + n2 + " rezultatas = " + sum;
+document.getElementById('b').innerHTML = 'Atimtis ' +  n1 + " ir " + n2 + " rezultatas = " + sub;
+document.getElementById('c').innerHTML = 'Sudėtis ' +  n1 + " ir " + n2 + " rezultatas = " + div;
+document.getElementById('d').innerHTML = 'Sudėtis ' +  n1 + " ir " + n2 + " rezultatas = " + multi;
